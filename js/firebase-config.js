8066ca493a5a1898a78956ef18d7828ca1bb9438
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-09-24 17:12:16 -0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA48HXFNhkFVWmcOnJZHb6cZp2X_QE-pYk",
-  authDomain: "quieniela360.firebaseapp.com",
-  databaseURL: "https://quieniela360-default-rtdb.firebaseio.com",
-  projectId: "quieniela360",
-  storageBucket: "quieniela360.firebasestorage.app",
-  messagingSenderId: "405592293769",
-  appId: "1:405592293769:web:d0c4026fd31875d09ba158",
-  measurementId: "G-53BKRGYYGV"
+  apiKey: "AIzaSyC8itN53SkagtwJPqw6XSllsUP8w7-o7d8",
+  authDomain: "quiniela-hydra.firebaseapp.com",
+  databaseURL: "https://quieniela-hydra-default-rtdb.firebaseio.com",
+  projectId: "quiniela-hydra",
+  storageBucket: "quiniela-hydra.firebasestorage.app",
+  messagingSenderId: "329849216739",
+  appId: "1:329849216739:web:83d23ff1424eb167172fd6",
+  measurementId: "G-FWDDQ5HQYV"
 };
 
 // Inicialización segura
@@ -17,3 +17,4 @@ if (!firebase.apps.length) {
 
 // Exporta el módulo de autenticación
 const auth = firebase.auth();
+
