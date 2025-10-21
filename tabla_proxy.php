331b73d59317efccf1e7a3cad23fef7aaf213e33
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-10-21 15:54:40 -0600

    Update tabla_proxy.php

--- tabla_proxy.php
+++ tabla_proxy.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = "https://api-football-standings.azharimm.dev/leagues/mex.1/standings";
+$url = "https://share.google/aimode/p2bkpcFPNtYhdaEgb";
 $response = @file_get_contents($url);
 if ($response === FALSE) {
   echo json_encode(["error" => "No se pudo obtener tabla"]);
