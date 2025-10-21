904a82ab27c53482ad105e05175a2f6679d9d545
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-10-21 15:40:03 -0600

    Update noticias_proxy.php

--- noticias_proxy.php
+++ noticias_proxy.php
@@ -5,6 +5,10 @@ header("Content-Type: application/json");
 $apiKey = "pub_55007f214e86a5dc8a6dcb9ee8350d1d4c9";
 $url = "https://newsdata.io/api/1/news?apikey={$apiKey}&q=futbol%20mexicano&country=mx&language=es";
 
-$response = file_get_contents($url);
-echo $response;
+$response = @file_get_contents($url);
+if ($response === FALSE) {
+  echo json_encode(["error" => "No se pudo obtener noticias"]);
+} else {
+  echo $response;
+}
 ?>
